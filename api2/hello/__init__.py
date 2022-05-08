ccd22ccdc9b803f07815640924940af266cbc60c
kshingaims <103156183+kshingaims@users.noreply.github.com>
2022-05-08 20:29:23 +0900

    Update __init__.py

--- api2/hello/__init__.py
+++ api2/hello/__init__.py
@@ -12,17 +12,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             req_body = req.get_json()
         except ValueError:
             pass
-    else:
-        name = {
-          "input": req_body.get('name'),
-          "message": "Hello, " + req_body.get('name') + ". This HTTP triggered function executed successfully."
-        }
+        else:
+            name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(
-          json.dumps(name),
-          mimetype="application/json" 
-        )
+        name = {
+            "input": req_body.get('name'),
+            "message": "Hello, " + req_body.get('name') + ". This HTTP triggered function executed successfully."
+        }
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
